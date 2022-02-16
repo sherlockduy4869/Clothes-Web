946ebe3fd795136b85bfd78020b850991526da8e
Duy-Kun <sherlockduy2001@gmail.com>
2022-02-16 19:06:55 +0700

    Create and completed sticky area, completed footer area

--- Web-Clothes-FullStack/Resources/js/script.js
+++ Web-Clothes-FullStack/Resources/js/script.js
@@ -1,30 +1,48 @@
+$(document).ready(
 
-const imgPosition = document.querySelectorAll(".aspect-ratio-169 img")
-const imgContainer = document.querySelector(".aspect-ratio-169")
-let index = 0
-let imgNumber = imgPosition.length
-const dotItem = document.querySelectorAll(".dot")
-//console.log(imgPosition)
-//abc
-imgPosition.forEach(function(image, index){
-    image.style.left = index*100 + "%"
-    dotItem[index].addEventListener("click",function(){
-    slider(index)    
-    })
-})
+    function(){
+        //sticky nav
+        $('.app-container').waypoint(
+            function(direction){
+                if(direction== "down"){
+                    $('header').addClass('sticky');
+                }
+                else{
+                    $('header').removeClass('sticky');
+                }
+            },{
+                offset: '730px'
+            }
+        )
 
-function imgSlide(){
-    index++;
-    if(index>=imgNumber){
-        index = 0
+        const imgPosition = document.querySelectorAll(".aspect-ratio-169 img")
+        const imgContainer = document.querySelector(".aspect-ratio-169")
+        let index = 0
+        let imgNumber = imgPosition.length
+        const dotItem = document.querySelectorAll(".dot")
+        //console.log(imgPosition)
+        //abc
+        imgPosition.forEach(function(image, index){
+            image.style.left = index*100 + "%"
+            dotItem[index].addEventListener("click",function(){
+            slider(index)    
+            })
+        })
+
+        function imgSlide(){
+            index++;
+            if(index>=imgNumber){
+                index = 0
+            }
+            slider(index)
+        }   
+
+        function slider(index){
+            imgContainer.style.left = "-" +index*100+ "%"
+            const dotActive = document.querySelector(".active")
+            dotActive.classList.remove("active")
+            dotItem[index].classList.add("active")
+        }
+        setInterval(imgSlide,5000)
     }
-    slider(index)
-}
-
-function slider(index){
-    imgContainer.style.left = "-" +index*100+ "%"
-    const dotActive = document.querySelector(".active")
-    dotActive.classList.remove("active")
-    dotItem[index].classList.add("active")
-}
-setInterval(imgSlide,5000)
+)
