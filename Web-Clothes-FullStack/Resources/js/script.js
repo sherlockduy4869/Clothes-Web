0a1dac88b914871046944eedd4067ca49c9c7159
Duy-Kun <sherlockduy2001@gmail.com>
2022-03-06 21:19:27 +0700

    Done product section

--- Web-Clothes-FullStack/Resources/js/script.js
+++ Web-Clothes-FullStack/Resources/js/script.js
@@ -14,7 +14,9 @@ $(document).ready(
                 offset: '730px'
             }
         )
+        
 
+        //Slider
         const imgPosition = document.querySelectorAll(".aspect-ratio-169 img")
         const imgContainer = document.querySelector(".aspect-ratio-169")
         let index = 0
@@ -52,6 +54,39 @@ $(document).ready(
             })
             
         })
+
+        //Product
+        const bigImg = document.querySelector(".product-content-left-big-img img")
+        const smallImg = document.querySelectorAll(".product-content-left-small-img img")
+        smallImg.forEach(function(imgItem,X){
+            imgItem.addEventListener("click",function(){
+                bigImg.src = imgItem.src
+            })
+        })
+
+        const baoquan = document.querySelector(".baoquan")
+        const chitiet = document.querySelector(".chitiet")
+
+        if(baoquan){
+            baoquan.addEventListener("click",function(){
+                document.querySelector(".product-content-right-bottom-top-content-chitiet").style.display = "none"
+                document.querySelector(".product-content-right-bottom-top-content-baoquan").style.display = "block"
+            })
+        }
+
+        if(chitiet){
+            chitiet.addEventListener("click",function(){
+                document.querySelector(".product-content-right-bottom-top-content-baoquan").style.display = "none"
+                document.querySelector(".product-content-right-bottom-top-content-chitiet").style.display = "block"
+            })
+        }
+
+        const btn = document.querySelector(".product-content-right-bottom-top")
+        if(btn){
+            btn.addEventListener("click", function(){
+                document.querySelector(".product-content-right-bottom-top-content-big").classList.toggle("activeB")
+            })
+        }
     }
 
 )
